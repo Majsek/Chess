f2da95e144e55d290091f87271be554a3b9f6ccf
Majsek <majsek555@gmail.com>
2020-11-13 00:36:42 +0100

    OnClick funkce

--- Scripts/Bishop.gd
+++ Scripts/Bishop.gd
@@ -13,3 +13,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+
+
--- Scripts/Figure.gd
+++ Scripts/Figure.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 var color_
+var selected_
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
@@ -23,3 +24,23 @@ func init(color):
 		$RigidBody/MeshInstance.set_surface_material(0, preload("res://Materials/white_material.tres"))
 func getColor():
 	return color_
+
+func select(selected):
+	selected_ = selected
+	if selected == true:
+		$RigidBody/MeshInstance.set_surface_material(0, preload("res://Materials/selected_material.tres"))
+	else:
+		init(getColor())
+
+func _on_RigidBody_input_event(camera: Node, event: InputEvent, click_position: Vector3, click_normal: Vector3, shape_idx: int) -> void:
+	if event.is_pressed():
+		var name = get_name()
+		var instance = get_instance_id()
+		var spatial = get_parent_spatial()
+		print(name)
+		print(instance)
+		print(spatial)
+#		set_translation(Vector3(3-10.5,10,3-10.5))
+		print (color_)
+		select(true)
+		print(selected_)
